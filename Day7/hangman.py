738df5bb275bb37362c3cad5ca10b11fcd142687
Jayasurya-Marasani <jayasuryamarasani@gmail.com>
2024-12-24 15:58:12 +0530

    Day 7 Added

---
+++ Day7/hangman.py
@@ -0,0 +1,331 @@
+import os
+import random
+os.system('cls')
+word_list = [
+  "abruptly",
+  "absurd",
+  "abyss",
+  "affix",
+  "askew",
+  "avenue",
+  "awkward",
+  "axiom",
+  "azure",
+  "bagpipes",
+  "bandwagon",
+  "banjo",
+  "bayou",
+  "beekeeper",
+  "bikini",
+  "blitz",
+  "blizzard",
+  "boggle",
+  "bookworm",
+  "boxcar",
+  "boxful",
+  "buckaroo",
+  "buffalo",
+  "buffoon",
+  "buxom",
+  "buzzard",
+  "buzzing",
+  "buzzwords",
+  "caliph",
+  "cobweb",
+  "cockiness",
+  "croquet",
+  "crypt",
+  "curacao",
+  "cycle",
+  "daiquiri",
+  "dirndl",
+  "disavow",
+  "dizzying",
+  "duplex",
+  "dwarves",
+  "embezzle",
+  "equip",
+  "espionage",
+  "euouae",
+  "exodus",
+  "faking",
+  "fishhook",
+  "fixable",
+  "fjord",
+  "flapjack",
+  "flopping",
+  "fluffiness",
+  "flyby",
+  "foxglove",
+  "frazzled",
+  "frizzled",
+  "fuchsia",
+  "funny",
+  "gabby",
+  "galaxy",
+  "galvanize",
+  "gazebo",
+  "giaour",
+  "gizmo",
+  "glowworm",
+  "glyph",
+  "gnarly",
+  "gnostic",
+  "gossip",
+  "grogginess",
+  "haiku",
+  "haphazard",
+  "hyphen",
+  "iatrogenic",
+  "icebox",
+  "injury",
+  "ivory",
+  "ivy",
+  "jackpot",
+  "jaundice",
+  "jawbreaker",
+  "jaywalk",
+  "jazziest",
+  "jazzy",
+  "jelly",
+  "jigsaw",
+  "jinx",
+  "jiujitsu",
+  "jockey",
+  "jogging",
+  "joking",
+  "jovial",
+  "joyful",
+  "juicy",
+  "jukebox",
+  "jumbo",
+  "kayak",
+  "kazoo",
+  "keyhole",
+  "khaki",
+  "kilobyte",
+  "kiosk",
+  "kitsch",
+  "kiwifruit",
+  "klutz",
+  "knapsack",
+  "larynx",
+  "lengths",
+  "lucky",
+  "luxury",
+  "lymph",
+  "marquis",
+  "matrix",
+  "megahertz",
+  "microwave",
+  "mnemonic",
+  "mystify",
+  "naphtha",
+  "nightclub",
+  "nowadays",
+  "numbskull",
+  "nymph",
+  "onyx",
+  "ovary",
+  "oxidize",
+  "oxygen",
+  "pajama",
+  "peekaboo",
+  "phlegm",
+  "pixel",
+  "pizazz",
+  "pneumonia",
+  "polka",
+  "pshaw",
+  "psyche",
+  "puppy",
+  "puzzling",
+  "quartz",
+  "queue",
+  "quips",
+  "quixotic",
+  "quiz",
+  "quizzes",
+  "quorum",
+  "razzmatazz",
+  "rhubarb",
+  "rhythm",
+  "rickshaw",
+  "schnapps",
+  "scratch",
+  "shiv",
+  "snazzy",
+  "sphinx",
+  "spritz",
+  "squawk",
+  "staff",
+  "strength",
+  "strengths",
+  "stretch",
+  "stronghold",
+  "stymied",
+  "subway",
+  "swivel",
+  "syndrome",
+  "thriftless",
+  "thumbscrew",
+  "topaz",
+  "transcript",
+  "transgress",
+  "transplant",
+  "triphthong",
+  "twelfth",
+  "twelfths",
+  "unknown",
+  "unworthy",
+  "unzip",
+  "uptown",
+  "vaporize",
+  "vixen",
+  "vodka",
+  "voodoo",
+  "vortex",
+  "voyeurism",
+  "walkway",
+  "waltz",
+  "wave",
+  "wavy",
+  "waxy",
+  "wellspring",
+  "wheezy",
+  "whiskey",
+  "whizzing",
+  "whomever",
+  "wimpy",
+  "witchcraft",
+  "wizard",
+  "woozy",
+  "wristwatch",
+  "wyvern",
+  "xylophone",
+  "yachtsman",
+  "yippee",
+  "yoked",
+  "youthful",
+  "yummy",
+  "zephyr",
+  "zigzag",
+  "zigzagging",
+  "zilch",
+  "zipper",
+  "zodiac",
+  "zombie",
+]
+h_list = [
+'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''',
+
+'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========   
+''',
+
+'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''',
+
+'''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========
+''',
+
+'''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========
+''',
+'''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========
+'''
+,
+
+'''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========
+''']
+
+print('''
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \\ / _' | '_ \\ / _' | '_ ' _ \\ / _' | '_ \\ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                    __/ |                      
+                   |___/
+''')
+
+
+choosen_word = word_list[random.randint(0,len(word_list)-1)]
+display = ['_' for i in range(len(choosen_word))]
+counter = 0
+lives = 6
+
+
+while (''.join(display) != choosen_word) and counter < 6:
+    print(f"word to Guess : {''.join(display)}")
+    guess = input('Guess a Letter: ').lower()
+    flag = -1
+    for i in range(len(choosen_word)):
+        if guess == choosen_word[i]:
+            flag = 1
+            if choosen_word[i] ==display[i]:
+                continue
+            else:
+                display[i] = choosen_word[i]
+                break
+    print(''.join(display))
+    print(h_list[counter])
+    print(f"*********************** {lives-counter-1}/6 Lives Left**********************")
+    if flag != 1:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        counter += 1
+
+if (''.join(display) == choosen_word):
+    print(f"*********************** YOU WON!! **********************")
+else:
+    print(f"*********************** IT WAS {choosen_word}!! YOU LOSE **********************")
+
