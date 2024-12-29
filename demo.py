e4696936a993d08be64610ab7278f6b7bc45045e
Jayasurya-Marasani <jayasuryamarasani@gmail.com>
2024-12-29 22:08:51 +0530

    Day 16 Updated

--- demo.py
+++
@@ -1,9 +0,0 @@
-# Example array
-arr = [5, 2, 9, 1, 5, 6]
-
-# Creating a new sorted array
-sorted_arr = sorted(arr)
-
-# Print the original and sorted arrays
-print("Original array:", arr)
-print("Sorted array:", sorted_arr)
