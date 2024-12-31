b8fbab8082c2ba05d9e2dc6a81339dfbd6e8bf0f
Jayasurya-Marasani <jayasuryamarasani@gmail.com>
2024-12-31 20:30:12 +0530

    Day 18 Updated

--- Day18/demo2.py
+++ Day18/demo2.py
@@ -11,3 +11,16 @@
 # tim = t.Turtle()
 
 # if there are modules that are not avaialbe in your standard library, then you can use pip to install the modules that are needed. 
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+for i in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+screen.exitonclick()
