23efd38bd0aee038d854696755aad65e97de9a74
Jayasurya-Marasani <jayasuryamarasani@gmail.com>
2024-12-31 20:21:17 +0530

    Day 18 Added

---
+++ Day18/demo2.py
@@ -0,0 +1,13 @@
+# from turtle import Turtle
+# tim = Turtle()
+
+# import turtle
+# tim = turtle.Turtle()
+
+# from turtle import *
+# tim = turtle.Turtle()
+
+# from turtle import as t
+# tim = t.Turtle()
+
+# if there are modules that are not avaialbe in your standard library, then you can use pip to install the modules that are needed. 
