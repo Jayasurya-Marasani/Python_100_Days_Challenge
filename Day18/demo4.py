31acfe115fd515108d4c281780ea199dd0db180e
Jayasurya Marasani <jayasuryamarasani@gmail.com>
2024-12-31 22:52:11 +0530

    Day 18 Updated

--- Day18/demo4.py
+++ Day18/demo4.py
@@ -28,13 +28,18 @@ colors = ["red", "blue", "green", "orange", "purple", "pink", "yellow", "cyan"]
 #         tim.left(90)
 #         tim.left(90)
 #         tim.forward(50)
-    
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    return (r, g, b)
 directions = [0, 90, 180, 270]
 tim.speed(300)
 for _ in range(200):
     tim.speed(20)
-    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    tim.pencolor(color)
+    # color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    tim.pencolor(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
