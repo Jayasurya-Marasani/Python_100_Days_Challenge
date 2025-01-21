837ec24e6357a13cef29e3fcc7c8b2e4e0cbfc6b
Jayasurya-Marasani <jayasuryamarasani@gmail.com>
2025-01-21 17:07:32 +0530

    Day 45 Updated

--- Day45/Starting Code - 100 movies to watch start/main.py
+++ Day45/Starting Code - 100 movies to watch start/main.py
@@ -4,5 +4,17 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+response = requests.get(url = URL)
+movies_web_page = response.text
 
+soup = BeautifulSoup(movies_web_page, "html.parser")
 
+h3_tags = soup.find_all(name = "h3", class_ = "title")
+print(h3_tags)
+h3_tags_movies = [tag.getText() for tag in h3_tags]
+h3_tags_movies = h3_tags_movies[::-1]
+print(h3_tags_movies)
+
+with open("movies.txt", "a", encoding="utf-8") as file:
+    for movie in h3_tags_movies:
+        file.write(movie + "\n")
